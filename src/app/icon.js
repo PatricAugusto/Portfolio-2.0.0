f2ea89dff1fbc00efce1afb191ed1095cd6df84f
Patric Augusto <druidacanabico@gmail.com>
2025-09-30 19:37:34 -0300

    fix: Resolução final do erro de build do favicon simplificando o componente icon.js

--- src/app/icon.js
+++ src/app/icon.js
@@ -1,37 +1,11 @@
-// src/app/icon.js
-
 import { FaRobot } from 'react-icons/fa';
-import { createElement } from 'react';
-import { renderToString } from 'react-dom/server';
-import { ImageResponse } from 'next/server';
-
-export const size = {
-  width: 32,
-  height: 32,
-};
-
-export const contentType = 'image/png';
 
 export default function Icon() {
-  const svgString = renderToString(createElement(FaRobot, { size: 32, color: 'white' }));
-  
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 24,
-          background: 'black',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        dangerouslySetInnerHTML={{ __html: svgString }}
-      />
-    ),
-    {
-      ...size,
-    }
+  return (
+    <FaRobot 
+      size={32} 
+      color="white" 
+      style={{ background: 'black', borderRadius: '50%' }}
+    />
   );
 }
